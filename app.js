cfbcac25dac25fd81a11a2a112dd815272e0cd54
Barr Covrigaru <barr@covrigaru.com>
2019-01-18 13:44:17 -0700

    halfway deploying

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 const env = require("dotenv".config())
 
+app.listen()
+
 app.use(express.static('public'))
 
 const data = require("./data.json")
@@ -28,7 +30,5 @@ app.use((err, req, res, next) => {
 app.use((req, res, next) => {
   res.status(404).json({error: {message: "Not found"}})
 })
-
-app.listen(env)
 
 module.exports = app
