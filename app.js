8d219389d4e1c9447a94a7844616aeea39c9f0f9
Barr Covrigaru <barr@covrigaru.com>
2019-01-18 13:54:29 -0700

    deployment works

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.get("/:first", (req, res, next) => {
   filteredFirst ? res.status(200).send(filteredFirst) : res.status(404).json({error: {message: "Not found"}})
 })
 
+app.post("/:first", (req, res, next) => {
+  const first = req.params.first
+  const filteredFirst = data.people.filter(person => person.first_name === first)
+  filteredFirst ? res.status(200).send(filteredFirst) : res.status(404).json({error: {message: "Not found"}})
+})
+
 app.use((err, req, res, next) => {
   const status = err.status || 500
   res.status(status).json({error:err})
