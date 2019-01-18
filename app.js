ec7961815bc8d0219366aa4b06ae086cb44c0413
Barr Covrigaru <barr@covrigaru.com>
2019-01-18 11:53:53 -0700

    no to deploy..

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ app.use(express.static('public'))
 
 const data = require("./data.json")
 
-// const parser = require('body-parser')
-// app.use(parser.json())
-
-app.get("/", (req, res) => res.send(data))
+app.get("/", (req, res) => {
+  res.status(200).send({
+    "message": "Success!",
+    "data": data
+  })
+})
+  
+app.get("/:first", (req, res, next) => {
+  const first = req.params.first
+  const filteredFirst = data.people.filter(person => person.first_name === first)
+  filteredFirst ? res.status(200).send(filteredFirst) : res.status(404).json({error: {message: "Not found"}})
+})
 
 app.use((err, req, res, next) => {
   const status = err.status || 500
