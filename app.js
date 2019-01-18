d46f394ecaeea0d48a01ca7d24f23129c040bfc7
Barr Covrigaru <barr@covrigaru.com>
2019-01-18 13:42:35 -0700

    halfway deploying

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const env = require("dotenv".config())
 
 app.use(express.static('public'))
 
@@ -28,7 +29,6 @@ app.use((req, res, next) => {
   res.status(404).json({error: {message: "Not found"}})
 })
 
-const listener = () => console.log(`Listening on port ${port}!`)
-app.listen(port, listener)
+app.listen(env)
 
 module.exports = app
