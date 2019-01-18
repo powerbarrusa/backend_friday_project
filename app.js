17f5a95d9e60ee12478267e60647838ffdd352f6
Barr Covrigaru <barr@covrigaru.com>
2019-01-18 13:49:07 -0700

    halfway deploying

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
-const env = require("dotenv".config())
+const env = require("dotenv").config()
 
 app.use(express.static('public'))
 
